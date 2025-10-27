2de26f6c44eebf6d90cc44a57cdef2c48af4396d
Enrique Simon <Enriquejsimonii@gmail.com>
2025-10-26 23:29:34 -0500

    Final Submissions for Tic Tac Toe

--- ticTacToe/src/ticTacToe/TicTacToe.java
+++ ticTacToe/src/ticTacToe/TicTacToe.java
@@ -182,7 +182,6 @@ public class TicTacToe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		startGame();
 	}
 
 }
---
+++ ticTacToe/src/ticTacToe/TicTacToeApp.java
@@ -0,0 +1,11 @@
+package ticTacToe;
+import ticTacToe.TicTacToe;
+
+public class TicTacToeApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TicTacToe.startGame();
+	}
+
+}
